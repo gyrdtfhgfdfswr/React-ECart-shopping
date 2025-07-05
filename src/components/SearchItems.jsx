f3bfb90c39e05ed2c56b2cf6e3772cf4ff64a11e
Sahil Divekar <sahild7588@gmail.com>
2025-07-05 11:33:18 +0530

    React-Shopping-Cart-App

--- src/components/SearchItems.jsx
+++ src/components/SearchItems.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { items } from './Data';
-import Product from './product';
+import Product from './Product ';
 const SearchItems = ({cart,setCart}) => {
 
   const {term}=useParams();
